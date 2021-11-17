938fc25dabfa39b38cab1f8a56158464722a251a
OEntusiasta <89933774+OEntusiasta@users.noreply.github.com>
2021-11-17 11:48:03 -0300

    bootstrap

--- view/Login.php
+++ view/Login.php
@@ -1,53 +1,44 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <?php
-       include("../view/Layout_generico/Header_Generico.php");
-    ?> 
+    include("../view/Layout_generico/Header_Generico.php");
+    ?>
 
     <title>Login</title>
-    <link href="../view/Css/tudo.css" rel="stylesheet"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="p-3 mb-2 bg-info text-white">
 
+    <div class="container">
+        <form method="POST" action="/login">
+            <div class="mb-3">
+                <h1>Faça seu login </h1>
+                <label for="email" class="form-label">E-mail</label>
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Digite seu E-mail" size="30" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">Seu email nunca será compartilhado</div>
             </div>
-        </nav>
-    </section>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Digite sua senha" size="30">
+            </div>
+            <div class="btn-options col-2 mx-auto">
+                <button class="btn btn-outline-primary" type="submit" class="btn-info-book-log">Entrar</button>
+            </div>
+        </form>
+        <form method="post" action="/register">
+            <div class="btn-options col-2 mx-auto">
+                <button class="btn btn-primary" type="submit" class="btn-info-book-reg"> Registre-se</button>
+            </div>
+        </form>
+    </div>
 
-    <section>
-        <div class="container">
-            <fieldset>
-                <form method="POST" action="/login">
-                    <div class="input-login">
-                        <h1>Faça seu login </h1>
-                        <div class="campos">
-                            <label for="email">E-mail</label>
-                            <input type="email" name="email" id="email" title="Digite seu E-mail" size="30" autofocus />
-                        </div>
-
-                        <div class="campos">
-                            <label for="password">Senha</label>
-                            <input type="password" name="password" id="password" title="Digite sua senha" size="30" />
-                        </div>
-                        <div class="btn-options">
-                   
-                            <button type="submit" class="btn-info-book-log" > Entrar </button>
-                        </div>
-                    </div>
-                </form>
-                <form method="post" action="/register">
-                    <div class="btn-options">
-                        <button type="submit" class="btn-info-book-reg"> Registre-se</button>
-                    </div>
-                </form>
-            </fieldset>
-        </div>
-    </section>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 <?php
-                    include("./view/Layout_generico/Footer_Generico.php");
-                ?>
+include("./view/Layout_generico/Footer_Generico.php");
+?>
 
 </html>
