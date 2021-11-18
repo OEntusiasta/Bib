605101b98ca8fe5660efa01eac108d8387d98e00
OEntusiasta <89933774+OEntusiasta@users.noreply.github.com>
2021-11-18 14:54:09 -0300

    Usuario bonito no boots

--- view/User/Emprestimos.php
+++ view/User/Emprestimos.php
@@ -1,67 +1,105 @@
-<!DOCTYPE html>
-<html lang="en">
-<?php
-include("../Layout_generico/Header_Generico.php");
-?>
-<link href="../view/Css/Home_page.css" rel="stylesheet">
-<title>Empréstimos</title>
+<html>
+
+<head>
+    <?php
+    include("../Layout_generico/Header_Generico.php");
+    ?>
+    <title>Empréstimos</title>
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <link href="../view/Css/InsideUser.css" rel="stylesheet">
 </head>
 
 <body>
-    <?php
-    include("./resources/views/layout/nav.php");
-    ?>
-    <ul class="items">
-        <li><a href="/User/hp">Home</a></li>
-        <li><a href="/User/Emprestimos">Empréstimos</a></li>
-        <li><a href="/client/historic">Histórico</a></li>
-        <li><a href="/client/data_user">Dados</a></li>
-        <li><a href="/">Sair</a></li>
-    </ul>
-    </div>
-    </nav>
-    </section>
 
-    <section>
+    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
         <div class="container">
-            <div class="section-books">
-                <div class="title-type-books">
-                    <h1>Livros emprestados</h1>
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-2">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/User/hp">Biblioteca Online</a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbar-collapse-2">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="/User/hp">Home</a></li>
+                    <li><a href="/User/Emprestimos">Empréstimos</a></li>
+                    <li><a href="/client/historic">Histórico</a></li>
+                    <li><a href="/client/data_user"><span class=" glyphicon glyphicon-user"></span>Meus Dados</a></li>
+                    <li>
+                        <a class="btn btn-default btn-outline btn-circle collapsed" data-toggle="collapse" href="/" aria-expanded="false" aria-controls="nav-collapse2"><span class="glyphicon glyphicon-log-in"></span>Sair</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
+
+
+
+    <div class="container">
+        <div class="mensagem">
+            <h1>Livros emprestados</h1>
+        </div>
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Livro</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Autor</th>
+                    <th scope="col">Data da locação: </th>
+                    <th scope="col">Data da entrega: </th>
+                    <th scope="col">Devolução</th>
+
+
+                </tr>
+            </thead>
+            <tbody>
+                <div class="section-books">
+                    <div class="type-books">
+                        <?php
+                        foreach ($loans as $userloan) {
+                            if ($userloan['user_id'] == $_SESSION['id']) {
+                        ?>
+                                <tr>
+                                    <td><img src="../view/Imagens//livro-icon-png-3.png" style="width: 3vw">
+                                        <img</td>
+                                    <td>
+                                        <?php if ($book = (new Book())->find($userloan['book_id'])) { ?>
+                                            <b><?= $book['name'] ?></b>
+                                    </td>
+                                    <td>
+                                        <?php if ($book = (new Book())->find($userloan['book_id'])) { ?>
+                                            <b><?= $book['author'] ?><b>
+
+                                    </td>
+                                    <td><?= $userloan['date_start'] ?> </td>
+                                    <td></b><?= $userloan['date_end'] ?></td>
+                                    <td>
+                                        <form method="post" action="/client/devolution">
+                                            <input type="hidden" name="id" value="<?= $userloan['id'] ?>">
+                                            <input type="hidden" name="book_id" value="<?= $book['id'] ?>">
+                                            <button class="btn btn-primary" type="submit">Devolver</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                    </div>
                 </div>
-                <div class="type-books">
-                    <?php
-                    foreach ($loans as $userloan) {
-                        if ($userloan['user_id'] == $_SESSION['id']) {
-                    ?>
-                            <div class="books">
-                                <div class="title-type-books">
-                                    <?php if ($book = (new Book())->find($userloan['book_id'])) { ?>
-                                        <b><?= $book['name'] ?></b>
-                                </div>
-
-                                <img class="img-book" src="/view/Imagens/livro-icon-png-3.png" />
-                                <div class="author-book">
-                                    <?= $book['author'] ?>
-                                </div>
-                            <?php } ?>
-                            <div class="date-book">
-                                <label for="date_start"><b>Data da locação: </b><?= $userloan['date_start'] ?> </label>
-                            </div>
-                            <div class="date-book">
-                                <label for="date_end"><b>Data da entrega: </b><?= $userloan['date_end'] ?></label>
 
 
-                                <form method="post" action="/client/devolution">
-                                    <input type="hidden" name="id" value="<?= $userloan['id'] ?>">
-                                    <input type="hidden" name="book_id" value="<?= $book['id'] ?>">
-                                    <button class="btn-concluded" type="submit">Devolver</button>
-                                </form>
-                            </div>
-                    <?php }
-                    } ?>
-                            </div>
-                </div>
-    </section>
+        <?php }
+                                        } ?>
+<?php
+                            }
+                        } ?>
+
+            </tbody>
+        </table>
+    </div>
+
 
 </body>
 
--- view/User/hp.php
+++ view/User/hp.php
@@ -5,12 +5,13 @@
     include("../Layout_generico/Header_Generico.php");
     ?>
     <title>Home Page</title>
- 
+
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <link href="../view/Css/InsideUser.css" rel="stylesheet">
+    <link href="../view/Css/InsideUser.css" rel="stylesheet">  
 </head>
+
 <body>
 
     <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
@@ -24,7 +25,7 @@
                 </button>
                 <a class="navbar-brand" href="/User/hp">Biblioteca Online</a>
             </div>
-            <div class="collapse navbar-collapse"  id="navbar-collapse-2">
+            <div class="collapse navbar-collapse" id="navbar-collapse-2">
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="/User/hp">Home</a></li>
                     <li><a href="/User/Emprestimos">Empréstimos</a></li>
@@ -37,55 +38,62 @@
             </div>
     </nav>
 
-    <section>
-        <div class="container">
-            <div class="mensagem">
-                <h1><b> Bem Vindo a Biblioteca Online<b> <?= $_SESSION['name'] ?>  <h1>
-            </div>
-            <div class="section-books">
-                <div class="type-books">
-                    <?php
-                    foreach ($booksClient as $book) {
-                    ?>
 
+    <div class="container">
+        <div class="mensagem">
+            <h1><b> Bem Vindo a Biblioteca Online<b> <?= $_SESSION['name'] ?> <h1>
+        </div>
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Livro</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Autor</th>
+                    <th scope="col">Descrição</th>
+                    <th scope="col">Disponibilidade </th>
 
-                        <div class="books">
-                            <div class="title-type-books">
-                                <h1><?= $book['category'] ?></h1>
-                            </div>
+                </tr>
+            </thead>
+            <tbody>
+                <div class="section-books">
+                    <div class="type-books">
+                        <?php
+                        foreach ($booksClient as $book) {
+                        ?>
+                            <tr>
+                                <td><img src="../view/Imagens//livro-icon-png-3.png" style="width: 3vw" >
+                                <img</td>
+                                <td><?= $book['category'] ?></td>
+                                <td><?= $book['name'] ?></td>
+                                <td><?= $book['author'] ?></td>
+                                <td><?= $book['description'] ?></td>
+                                <td>
+                                    <div class="btn-options">
+                                        <?php
+                                        if (($book['status'] == 'pending') || ($book['status'] == 'unavailable')) {
 
-                            <img class="img-book" src="/view/Imagens/livro-icon-png-3.png" />
-                            <div class="title-book">
-                                <b><?= $book['name'] ?></b>
-                            </div>
-                            <div class="author-book">
-                                <b><?= $book['author'] ?></b>
-                            </div>
-                            <div class="description-book">
-                                <p><?= $book['description'] ?></p>
-                            </div>
+                                            echo "Status: " . $book['status'];
+                                        } else {
+                                        ?>
 
-                            <div class="btn-options">
-                                <?php
-                                if (($book['status'] == 'pending') || ($book['status'] == 'unavailable')) {
+                                            <form method="post" action="/client/store">
+                                                <input type="hidden" name="id" value="<?= $book['id'] ?>" />
+                                                <button class="btn btn-primary" type="submit">Emprestar</button>
+                                            </form>
 
-                                    echo "Status: " . $book['status'];
-                                } else {
-                                ?>
-
-                                    <form method="post" action="/client/store">
-                                        <input type="hidden" name="id" value="<?= $book['id'] ?>" />
-                                        <button class="btn-concluded" type="submit">Emprestar</button>
-                                    </form>
-
-                                <?php } ?>
-
-                            </div>
-                        </div>
-                    <?php } ?>
+                                        <?php } ?>
+                                </td>
+                            </tr>
+                    </div>
                 </div>
-            </div>
-    </section>
+            <?php } ?>
+    </div>
+    </tbody>
+    </table>
+    </div>
+
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
